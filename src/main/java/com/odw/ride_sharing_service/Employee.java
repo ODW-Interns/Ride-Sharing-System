7b4ea30b32ac5b45ca9b27f6af4104e73c0b8c4c
wesleydong <wesleydong@live.com>
2018-05-14 16:01:33 -0700

    Add Employee class

---
+++ src/main/java/com/odw/ride_sharing_service/Employee.java
@@ -0,0 +1,52 @@
+package com.odw.ride_sharing_service;
+
+public class Employee {
+	
+	private int employeeAge;
+	private int employeeId;
+	private int employeeRating;
+	private int employeeSalary;
+	private String employeeName;
+	private String employeeSex;
+	
+	//TODO: add private Car employeeCar;
+	
+	public int getEmployeeAge() {
+		return employeeAge;
+	}
+	public void setEmployeeAge(int employeeAge_) {
+		this.employeeAge = employeeAge_;
+	}
+	public int getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(int employeeId_) {
+		this.employeeId = employeeId_;
+	}
+	public int getEmployeeRating() {
+		return employeeRating;
+	}
+	public void setEmployeeRating(int employeeRating_) {
+		this.employeeRating = employeeRating_;
+	}
+	public int getEmployeeSalary() {
+		return employeeSalary;
+	}
+	public void setEmployeeSalary(int employeeSalary_) {
+		this.employeeSalary = employeeSalary_;
+	}
+	public String getEmployeeName() {
+		return employeeName;
+	}
+	public void setEmployeeName(String employeeName_) {
+		this.employeeName = employeeName_;
+	}
+	public String getEmployeeSex() {
+		return employeeSex;
+	}
+	public void setEmployeeGender(String employeeSex_) {
+		this.employeeSex = employeeSex_;
+	}
+
+}
+

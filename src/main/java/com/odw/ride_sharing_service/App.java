05e39e393883f85329a7557c4ea3a2656f12d5c8
Mark Constantine <MarkConstantConstantine@gmail.com>
2018-05-15 14:29:25 -0700

    Added Slf4j dependency

--- src/main/java/com/odw/ride_sharing_service/App.java
+++ src/main/java/com/odw/ride_sharing_service/App.java
@@ -1,13 +1,26 @@
 package com.odw.ride_sharing_service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    private Logger _logger;
+    
+    public App() {
+        _logger= LoggerFactory.getLogger(getClass().getSimpleName());
+    }
+    
+    public void run() {
+        if (_logger.isDebugEnabled())
+            _logger.debug("Hello World!");
+    }
+    
+    public static void main(String[] args) {
+        App a = new App();
+        a.run();
     }
 }
--- src/test/java/com/odw/ride_sharing_service/AppTest.java
+++ src/test/java/com/odw/ride_sharing_service/AppTest.java
@@ -33,6 +33,8 @@ public class AppTest
      */
     public void testApp()
     {
-        assertTrue( true );
+        //assertTrue( true );
+        App a = new App();
+        a.run();
     }
 }

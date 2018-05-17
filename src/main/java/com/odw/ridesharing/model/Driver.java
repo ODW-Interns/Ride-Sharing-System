1bcb7a0442056091083c94e3e4121d3f3d6e37cf
pete210339 <s.tanthma@gmail.com>
2018-05-17 14:24:09 -0700

    JavaDoc
    Document Driver class and Person class

--- src/main/java/com/odw/ridesharing/model/Driver.java
+++ src/main/java/com/odw/ridesharing/model/Driver.java
@@ -2,55 +2,120 @@ package com.odw.ridesharing.model;
 
 public class Driver extends InputType {
 
-    private static int nextID = 0;
+	private static int nextID = 0;
 
-    private int driverID;
-    private int carID;
-    private int rating;
-    private Person driver;
-    private boolean isAvailable;
-    
-    public Driver() {
-        this(new Person(), -1, false, -1);
-    }
-    
-    public Driver(Person driver_, int rating_, boolean isAvailable_, int carID_) {
-        setDriver(driver_);
-        setRating(rating_);
-        setIsAvailable(isAvailable_);
-        setCarID(carID_);
-        
-        driverID = Driver.nextID++;
-    }
+	private int driverID;
+	private int carID;
+	private int rating;
+	private Person driver;
+	private boolean isAvailable;
 
-    @Override
-    public Types getType() {
-        return InputType.Types.DRIVER;
-    }
-    
-    /* Setters and Getters */
-    public Person getDriver() {
-        return driver;
-    }
-    public void setDriver(Person driver_) {
-        driver = driver_;
-    }
-    public int getRating() {
-        return rating;
-    }
-    public void setRating(int rating_) {
-        rating = rating_;
-    }
-    public boolean getIsAvailable() {
-        return isAvailable;
-    }
-    public void setIsAvailable(boolean isAvailable_) {
-        isAvailable = isAvailable_;
-    }
-    public int getCarID() {
-        return carID;
-    }
-    public void setCarID(int carID_) {
-        carID = carID_;
-    }
+	public Driver() {
+		this(new Person(), -1, false, -1);
+	}
+
+	/*
+	 * Store the information of this driver
+	 * 
+	 * @param driver_ Driver's info and should includes Fistname, Lastname, Sex, and Age
+	 * 
+	 * @param rating_ Driver's rating
+	 * 
+	 * @param isAvaliable_ Driver's availability
+	 * 
+	 * @param carID_ Driver's unique car id
+	 */
+	public Driver(Person driver_, int rating_, boolean isAvailable_, int carID_) {
+		setDriver(driver_);
+		setRating(rating_);
+		setIsAvailable(isAvailable_);
+		setCarID(carID_);
+
+		driverID = Driver.nextID++;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see com.odw.ridesharing.model.InputType#getType() return types of driver
+	 */
+	@Override
+	public Types getType() {
+		return InputType.Types.DRIVER;
+	}
+
+	/*
+	 * Get the info of this person driver
+	 * 
+	 * @return driver this driver's info
+	 */
+	public Person getDriver() {
+		return driver;
+	}
+	
+	/*
+	 * Set person driver
+	 * 
+	 * @param driver_ This is driver's info
+	 * 				  It should includes Firstname, Lastname, Sex, and Age
+	 */
+	public void setDriver(Person driver_) {
+		driver = driver_;
+	}
+	
+	/*
+	 * Get the rating of this driver
+	 * 
+	 * @return rating driver's rating info
+	 */
+	public int getRating() {
+		return rating;
+	}
+	
+	/*
+	 * Set the rating of this driver
+	 * 
+	 * @param rating_ This is rating's info of the driver
+	 * 				  It should range from 1 to 5
+	 */
+	public void setRating(int rating_) {
+		rating = rating_;
+	}
+	
+	/*
+	 * Get the availability of this driver
+	 * 
+	 * @return isAvailable This driver's status
+	 */
+	public boolean getIsAvailable() {
+		return isAvailable;
+	}
+
+	/*
+	 * Set the status of the this driver
+	 * 
+	 * @param isAvailable This driver's status
+	 * 					  It should be true or false
+	 */
+	public void setIsAvailable(boolean isAvailable_) {
+		isAvailable = isAvailable_;
+	}
+
+	/*
+	 * Get the car id of this driver
+	 * 
+	 * @return carID The id of this person's car
+	 * 				 The id should be unique integer number
+	 */
+	public int getCarID() {
+		return carID;
+	}
+
+	/*
+	 * Set the car id of this driver
+	 * 
+	 * @param carID This is the driver's unique car id
+	 */
+	public void setCarID(int carID_) {
+		carID = carID_;
+	}
 }
--- src/main/java/com/odw/ridesharing/model/Person.java
+++ src/main/java/com/odw/ridesharing/model/Person.java
@@ -2,46 +2,104 @@ package com.odw.ridesharing.model;
 
 public class Person {
 
-    private String firstName;
-    private String lastName;
-    private String sex;
-    private int age;
-    
-    public Person() {
-        this("", "", "", -1);
-    }
-    
-    public Person(String firstName_, String lastName_, String sex_, int age_) {
-        setFirstName(firstName_);
-        setLastName(lastName_);
-        setSex(sex_);
-        setAge(age_);
-    }
-    
-    /* Setters and Getters */
-    public String getFirstName() {
-        return firstName;
-    }
-    public void setFirstName(String firstName_) {
-        firstName = firstName_;
-    }
-    public String getLastName() {
-        return lastName;
-    }
-    public void setLastName(String lastName_) {
-        lastName = lastName_;
-    }
-    public String getSex() {
-        return sex;
-    }
-    public void setSex(String sex_) {
-        sex = sex_;
-    }
-    public int getAge() {
-        return age;
-    }
-    public void setAge(int age_) {
-        age = age_;
-    }
-    
+	private String firstName;
+	private String lastName;
+	private String sex;
+	private int age;
+
+	public Person() {
+		this("", "", "", -1);
+	}
+
+	/*
+	 * Store the information of this person
+	 * 
+	 * @param firstName This person's first name
+	 * 
+	 * @param lastName_ This person's last name
+	 * 
+	 * @param sex_ This person's gender
+	 * 
+	 * @param age_ This person's age
+	 */
+	public Person(String firstName_, String lastName_, String sex_, int age_) {
+		setFirstName(firstName_);
+		setLastName(lastName_);
+		setSex(sex_);
+		setAge(age_);
+	}
+
+	/*
+	 * Get the first name of this person
+	 * 
+	 * @return firstName This person's first name
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/*
+	 * Set the first name of this person
+	 * 
+	 * @param firstName_ The first name of this person
+	 */
+	public void setFirstName(String firstName_) {
+		firstName = firstName_;
+	}
+
+	/*
+	 * Get the last name of this person
+	 * 
+	 * @return lastName This person's last name
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+	
+	/*
+	 * Set the last name of this person
+	 * 
+	 * @param lastName_ This person's last name
+	 */
+	public void setLastName(String lastName_) {
+		lastName = lastName_;
+	}
+	
+	/*
+	 * Get the gender of this person
+	 * 
+	 * @return sex This person's gender
+	 * 			   It should return "male" or "female"
+	 */
+	public String getSex() {
+		return sex;
+	}
+
+	/*
+	 * Set the gender of this person
+	 * 
+	 * @param sex_ This person's gender
+	 */
+	public void setSex(String sex_) {
+		sex = sex_;
+	}
+
+	/*
+	 * Get the age of this person
+	 * 
+	 * @return the person's age
+	 */
+	public int getAge() {
+		return age;
+	}
+
+	/*
+	 * Set the age of this person
+	 * 
+	 * @param age_ This person's age
+	 */
+	public void setAge(int age_) {
+		age = age_;
+	}
+
 }

bdb67ee9c8696dc8c6908ffe0f355e4f1e9ac1c4
pete210339 <s.tanthma@gmail.com>
2018-05-17 15:30:56 -0700

    Documented Location Class
    Documented Location Class

--- src/main/java/com/odw/ridesharing/model/Location.java
+++ src/main/java/com/odw/ridesharing/model/Location.java
@@ -8,7 +8,15 @@ public class Location {
     public Location() {
         this(0, 0);
     }
-    
+    /**
+     * Initialize a Location with longitude and latitude
+     * 
+     * Should be either origin location or destination location
+	 * 
+     * @param longitude_ This is the longitude of the location (x-coordinate)
+     * 
+     * @param latitude_ This is the latitude of the location (y-coordinate)
+     */
     public Location(double longitude_, double latitude_) {
         longitude = longitude_;
         latitude = latitude_;

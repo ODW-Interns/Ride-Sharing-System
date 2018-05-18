5f46ead39bc8273ec7eb927765673c61f78b8391
wesleydong <wesleydong@live.com>
2018-05-18 14:45:53 -0700

    CarFactory creates and assigns ID to Car

--- src/main/java/com/odw/ridesharing/service/CarFactory.java
+++ src/main/java/com/odw/ridesharing/service/CarFactory.java
@@ -9,17 +9,22 @@ import com.odw.ridesharing.model.Sedan;
 
 public class CarFactory {
 
-    public Car createCar(ArrayList<String> typeValues) {
+    private int nextCarID = 0;
+    
+    public Car createCar(ArrayList<String> typeValues) {        
         switch (typeValues.get(0)) {
             case Car.COUPE:
+                //nextCarID++;
                 return new Coupe(typeValues.get(1), typeValues.get(2), typeValues.get(3), 
-                        Integer.parseInt(typeValues.get(4)));
+                        Integer.parseInt(typeValues.get(4)), nextCarID++);
             case Car.SEDAN:
+                //nextCarID++;
                 return new Sedan(typeValues.get(1), typeValues.get(2), typeValues.get(3), 
-                        Integer.parseInt(typeValues.get(4)));
+                        Integer.parseInt(typeValues.get(4)), nextCarID++);
             case Car.SUV:
+                //nextCarID++;
                 return new SUV(typeValues.get(1), typeValues.get(2), typeValues.get(3), 
-                        Integer.parseInt(typeValues.get(4)));
+                        Integer.parseInt(typeValues.get(4)), nextCarID++);
             default:
                 return null;
         }

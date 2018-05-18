39f87581c5ad164a6f503d2d08a39e02c4b36e75
wesleydong <wesleydong@live.com>
2018-05-18 12:31:08 -0700

    Created Car interface

---
+++ src/main/java/com/odw/ridesharing/model/Car.java
@@ -0,0 +1,7 @@
+package com.odw.ridesharing.model;
+
+public interface Car {
+    public static final String COUPE = "coupe";
+    public static final String SEDAN = "sedan";
+    public static final String SUV = "suv";
+}

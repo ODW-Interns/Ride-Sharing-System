19b5db1998270565e139366e3d71c954109eaa0b
wesleydong <wesleydong@live.com>
2018-05-23 11:20:18 -0700

    Updated fields for createPickup in Controller and Factory

--- src/main/java/com/odw/ridesharing/service/PickupFactory.java
+++ src/main/java/com/odw/ridesharing/service/PickupFactory.java
@@ -14,11 +14,11 @@ public class PickupFactory {
      * @param typeValues_ 
      * 
      * NOTE: typeValues_ format (as an ArrayList)
-     *      Values: | carID | customerID | driverID | origin_x | origin_y | 
-     *      Index:  | 0     | 1          | 2        | 3        | 4        |
+     *      Values: | customerID | origin_x | origin_y | destination_x |
+     *      Index:  | 0          | 1        | 2        | 3             |
      *      
-     *      Values: | destination_x | destination_y|
-     *      Index:  | 5             | 6            |
+     *      Values: | destination_y | driverID | driverFirstName | driverLastName |
+     *      Index:  | 4             | 5        | 6               | 7              |
      *      
      * @return a Pickup object with a pickupID attached to it
      */
@@ -26,19 +26,21 @@ public class PickupFactory {
         
         // get the values from the ArrayList
         int _customerID = Integer.parseInt(typeValues_.get(0));
-        int _driverID = Integer.parseInt(typeValues_.get(1));
-        double _originLongitude = Double.parseDouble(typeValues_.get(2));
-        double _originLatitude = Double.parseDouble(typeValues_.get(3));
-        double _destinationLongitude = Double.parseDouble(typeValues_.get(4));
-        double _destinationLatitude = Double.parseDouble(typeValues_.get(5));
+        double _originLongitude = Double.parseDouble(typeValues_.get(1));
+        double _originLatitude = Double.parseDouble(typeValues_.get(2));
+        double _destinationLongitude = Double.parseDouble(typeValues_.get(3));
+        double _destinationLatitude = Double.parseDouble(typeValues_.get(4));
+        int _driverID = Integer.parseInt(typeValues_.get(5));
+        String _driverFirstName = typeValues_.get(6);
+        String _driverLastName = typeValues_.get(7);
         
         // set the origin and destination to be used for Pickup
         Location _origin = new Location(_originLongitude, _originLatitude);
         Location _destination = new Location(_destinationLongitude, _destinationLatitude);
         
         //create a new Pickup object
-        return new Pickup(nextPickupID++, _customerID, _driverID,
-                _origin, _destination);
+        return new Pickup(nextPickupID++, _customerID, _origin, _destination, 
+                _driverID,_driverFirstName, _driverLastName);
     }
 
 }

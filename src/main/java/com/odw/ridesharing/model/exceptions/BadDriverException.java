1c81b8b8bc475e1aafaa2ac55599bd06b5f361a5
pete210339 <s.tanthma@gmail.com>
2018-05-24 10:41:48 -0700

    Modified Exceptions
    Added functions to exception classes

--- src/main/java/com/odw/ridesharing/model/exceptions/BadDriverException.java
+++ src/main/java/com/odw/ridesharing/model/exceptions/BadDriverException.java
@@ -1,5 +1,44 @@
 package com.odw.ridesharing.model.exceptions;
 
-public class BadDriverException extends Exception{
+public class BadDriverException extends Exception {
+    
+    /**
+     *
+     */
+    public BadDriverException() {
+    }
+
+    /**
+     *
+     */
+    public BadDriverException(String message_) {
+        super(message_);
+
+    }
+
+    /**
+     *
+     */
+    public BadDriverException(Throwable cause_) {
+        super(cause_);
+
+    }
+
+    /**
+     *
+     */
+    public BadDriverException(String message_, Throwable cause_) {
+        super(message_, cause_);
+
+    }
+
+    /**
+     *
+     */
+    public BadDriverException(String message_, Throwable cause_, boolean enableSuppression_,
+            boolean writableStackTrace_) {
+        super(message_, cause_, enableSuppression_, writableStackTrace_);
+
+    }
 
 }
--- src/main/java/com/odw/ridesharing/model/exceptions/BadUserException.java
+++ src/main/java/com/odw/ridesharing/model/exceptions/BadUserException.java
@@ -1,5 +1,44 @@
 package com.odw.ridesharing.model.exceptions;
 
-public class BadUserException extends Exception{
+public class BadUserException extends Exception {
+    
+    /**
+     *
+     */
+    public BadUserException() {
+    }
+
+    /**
+     *
+     */
+    public BadUserException(String message_) {
+        super(message_);
+
+    }
+
+    /**
+     *
+     */
+    public BadUserException(Throwable cause_) {
+        super(cause_);
+
+    }
+
+    /**
+     *
+     */
+    public BadUserException(String message_, Throwable cause_) {
+        super(message_, cause_);
+
+    }
+
+    /**
+     *
+     */
+    public BadUserException(String message_, Throwable cause_, boolean enableSuppression_,
+            boolean writableStackTrace_) {
+        super(message_, cause_, enableSuppression_, writableStackTrace_);
+
+    }
 
 }

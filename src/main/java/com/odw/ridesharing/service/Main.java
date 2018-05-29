4938def0e4398576908ff51cc78c3f24b4ca84fc
Mark Constantine <MarkConstantConstantine@gmail.com>
2018-05-29 15:00:05 -0700

    Quick change to main

--- src/main/java/com/odw/ridesharing/service/Main.java
+++ src/main/java/com/odw/ridesharing/service/Main.java
@@ -7,12 +7,21 @@ public class Main {
      */
     public static void main(String[] args_) {
         CommandController _commandController = new CommandController();
+        
+        // ScheduleWithNoAvailableDriver.txt
         _commandController.processFile("src/main/resources/ScheduleWithNoAvailableDriver.txt", "|");
-    
+        
+        // Resetting the databases. (Note: This is not necessary)
         _commandController = new CommandController();
+   
+        // InvalidInputFormats.txt
         _commandController.processFile("src/main/resources/InvalidInputFormats.txt", "|");
-
+        // File found and read with no exceptions.
+      
+        // Resetting the databases.
         _commandController = new CommandController();
+      
+        // LargeInputExample.txt
         _commandController.processFile("src/main/resources/LargeInputExample.txt", "|");
     }
 

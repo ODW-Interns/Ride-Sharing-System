138292a5314c91fa94b490387ee37f15782828d0
Mark Constantine <MarkConstantConstantine@gmail.com>
2018-06-13 13:41:54 -0700

    Quick code refactoring

--- src/test/java/com/odw/ridesharing/tests/factories/UserFactoryTest.java
+++ src/test/java/com/odw/ridesharing/tests/factories/UserFactoryTest.java
@@ -1,13 +1,11 @@
 package com.odw.ridesharing.tests.factories;
 
 import static org.junit.Assert.*;
-import java.util.ArrayList;
 
 import org.junit.Test;
 
 import com.odw.ridesharing.exceptions.InvalidUserArgumentsException;
 import com.odw.ridesharing.factories.UserFactory;
-import com.odw.ridesharing.model.CarType;
 import com.odw.ridesharing.model.Customer;
 import com.odw.ridesharing.model.Driver;
 import com.odw.ridesharing.model.UserType;
@@ -23,17 +21,17 @@ public class UserFactoryTest {
      */
     @Test
     public void testBuildCustomer() {
-    	
         UserFactory _userFactory = new UserFactory();
-    	UserType _userType = UserType.valueOf("customer".toUpperCase());
-        String _firstName = "miley";
-        String _lastName = "cyrus";
-        String _sex = "female";
-        int _age = 25;
 
         // ---------------------------------------------
         // Creating a valid customer.
         try {
+            UserType _userType = UserType.valueOf("customer".toUpperCase());
+            String _firstName = "miley";
+            String _lastName = "cyrus";
+            String _sex = "female";
+            int _age = 25;
+            
             AbstractUser _shouldBeCustomer = _userFactory.buildUser(_userType, _firstName, _lastName, _sex, _age);
             assertTrue(_shouldBeCustomer instanceof Customer);
         } catch (InvalidUserArgumentsException e) {
@@ -47,15 +45,16 @@ public class UserFactoryTest {
     @Test
     public void testBuildDriver() {
         UserFactory _userFactory = new UserFactory();
-    	UserType _userType = UserType.valueOf("driver".toUpperCase());
-        String _firstName = "wesley";
-        String _lastName = "dong";
-        String _sex = "male";
-        int _age = 23;
 
         // ---------------------------------------------
         // Creating a valid driver.
         try {
+            UserType _userType = UserType.valueOf("driver".toUpperCase());
+            String _firstName = "wesley";
+            String _lastName = "dong";
+            String _sex = "male";
+            int _age = 23;
+            
             AbstractUser _shouldBeDriver = _userFactory.buildUser(_userType, _firstName, _lastName, _sex, _age);
             assertTrue(_shouldBeDriver instanceof Driver);
         } catch (InvalidUserArgumentsException e) {
@@ -78,8 +77,10 @@ public class UserFactoryTest {
             String _lastName =  "super";
             String _sex =  "bad";
             int _age = 22;
+            
             @SuppressWarnings("unused") // Suppressed because this variable is not needed.
             AbstractUser _unknownUserType = _userFactory.buildUser(_userType, _firstName, _lastName, _sex, _age);
+            
             fail("Built a user with invalid input without issues.");
         } catch (InvalidUserArgumentsException e) {
             // Execution SHOULD reach inside the catch statement.

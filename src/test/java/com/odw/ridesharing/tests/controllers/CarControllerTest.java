1832beb9d6d9c9b5a4de116c67805cfa6119ae0b
pete210339 <s.tanthma@gmail.com>
2018-06-14 10:44:16 -0700

    Modified CarFactoryTest and CarControllerTest
    Add more test cases to CarFactoryTest and CarControllerTest

--- src/test/java/com/odw/ridesharing/tests/controllers/CarControllerTest.java
+++ src/test/java/com/odw/ridesharing/tests/controllers/CarControllerTest.java
@@ -34,16 +34,53 @@ public class CarControllerTest {
 
 	/**
 	 * Tests CarController's createCar method. Ensures that a invalid car cannot be
-	 * created.
+	 * created when length is invalid.
 	 */
 	@Test
-	public void testCreateInvalidCar() {
+	public void testCreateInvalidCarLength() {
 		CarController _carController = new CarController();
 
 		// ---------------------------------------------
 		// Testing invalid car creation.
 		try {
 			_carController.createCar(createInvalidCarInfo());
+		} catch (InvalidCarArgumentsException e_) {
+
+		}
+	}
+
+	/**
+	 * Tests CarController's createCar method. Ensures that a invalid car cannot be
+	 * created when year is invalid.
+	 */
+	@Test
+	public void testCreateInvalidCarYear() {
+		CarController _carController = new CarController();
+
+		// ---------------------------------------------
+		// Testing invalid car creation.
+		try {
+			ArrayList<String> _invalidCarInfo = createInvalidCarInfo();
+			_invalidCarInfo.add("length");
+			_carController.createCar(_invalidCarInfo);
+		} catch (InvalidCarArgumentsException e_) {
+
+		}
+	}
+
+	/**
+	 * Tests CarController's createCar method. Ensures that a invalid car cannot be
+	 * created when year is invalid.
+	 */
+	@Test
+	public void testCreateInvalidCarType() {
+		CarController _carController = new CarController();
+
+		// ---------------------------------------------
+		// Testing invalid car creation.
+		try {
+			ArrayList<String> _invalidCarInfo = createInvalidCarTypeInfo();
+			_carController.createCar(_invalidCarInfo);
 		} catch (InvalidCarArgumentsException e_) {
 
 		}
@@ -83,10 +120,10 @@ public class CarControllerTest {
 
 	/**
 	 * Tests CarController's modifyCar method. Ensures that a invalid car cannot be
-	 * modified.
+	 * modified when input length is invalid.
 	 */
 	@Test
-	public void testModifyInvalidCar() {
+	public void testModifyInvalidCarLength() {
 		CarController _carController = new CarController();
 
 		// ---------------------------------------------
@@ -109,9 +146,191 @@ public class CarControllerTest {
 	}
 
 	/**
+	 * Tests CarController's modifyCar method. Ensures that a invalid car cannot be
+	 * modified when carID is invalid.
+	 */
+	@Test
+	public void testModifyInvalidCarID() {
+		CarController _carController = new CarController();
+
+		// ---------------------------------------------
+		// Creating a valid car to later be modified.
+		try {
+			_carController.createCar(createValidCarInfo());
+		} catch (InvalidCarArgumentsException e_) {
+			fail("Error creating a valid car.");
+		}
+
+		// ---------------------------------------------
+		// Testing invalid car modification.
+		// Testing CarID does not exist
+		ArrayList<String> _coupeNewInfo = new ArrayList<String>();
+		_coupeNewInfo.add("6"); // ID
+		_coupeNewInfo.add("coupe");
+		_coupeNewInfo.add("toyota");
+		_coupeNewInfo.add("trueno");
+		_coupeNewInfo.add("black");
+		_coupeNewInfo.add("1986");
+
+		try {
+			_carController.modifyCar(_coupeNewInfo);
+		} catch (Exception e_) {
+
+		}
+
+		// ---------------------------------------------
+		// Testing CarID is invalid
+		_coupeNewInfo.set(0, "5r5");
+		try {
+			_carController.modifyCar(_coupeNewInfo);
+		} catch (Exception e_) {
+
+		}
+	}
+
+	/**
+	 * Tests CarController's modifyCar method. Ensures that a invalid car cannot be
+	 * modified when carType is invalid.
+	 */
+	@Test
+	public void testModifyInvalidCarType() {
+		CarController _carController = new CarController();
+
+		// ---------------------------------------------
+		// Creating a valid car to later be modified.
+		try {
+			_carController.createCar(createValidCarInfo());
+		} catch (InvalidCarArgumentsException e_) {
+			fail("Error creating a valid car.");
+		}
+
+		// ---------------------------------------------
+		// Testing invalid car modification.
+		ArrayList<String> _invalidModifyInfo = new ArrayList<String>();
+		_invalidModifyInfo.add("0"); // ID
+		_invalidModifyInfo.add("sedan");
+		_invalidModifyInfo.add("toyota");
+		_invalidModifyInfo.add("trueno");
+		_invalidModifyInfo.add("black");
+		_invalidModifyInfo.add("1986");
+		try {
+			_carController.modifyCar(_invalidModifyInfo);
+		} catch (Exception e_) {
+
+		}
+	}
+
+	/**
+	 * Tests CarController's modifyCar method. Ensures that a invalid car cannot be
+	 * modified when car make is invalid.
+	 */
+	@Test
+	public void testModifyInvalidCarMake() {
+		CarController _carController = new CarController();
+
+		// ---------------------------------------------
+		// Creating a valid car to later be modified.
+		try {
+			_carController.createCar(createValidCarInfo());
+		} catch (InvalidCarArgumentsException e_) {
+			fail("Error creating a valid car.");
+		}
+
+		// ---------------------------------------------
+		// Testing invalid car modification.
+		ArrayList<String> _invalidModifyInfo = new ArrayList<String>();
+		_invalidModifyInfo.add("0"); // ID
+		_invalidModifyInfo.add("coupe");
+		_invalidModifyInfo.add("nissan");
+		_invalidModifyInfo.add("trueno");
+		_invalidModifyInfo.add("black");
+		_invalidModifyInfo.add("1986");
+		try {
+			_carController.modifyCar(_invalidModifyInfo);
+		} catch (Exception e_) {
+
+		}
+	}
+
+	/**
+	 * Tests CarController's modifyCar method. Ensures that a invalid car cannot be
+	 * modified when car model is invalid.
+	 */
+	@Test
+	public void testModifyInvalidCarModel() {
+		CarController _carController = new CarController();
+
+		// ---------------------------------------------
+		// Creating a valid car to later be modified.
+		try {
+			_carController.createCar(createValidCarInfo());
+		} catch (InvalidCarArgumentsException e_) {
+			fail("Error creating a valid car.");
+		}
+
+		// ---------------------------------------------
+		// Testing invalid car modification.
+		ArrayList<String> _invalidModifyInfo = new ArrayList<String>();
+		_invalidModifyInfo.add("0"); // ID
+		_invalidModifyInfo.add("coupe");
+		_invalidModifyInfo.add("toyota");
+		_invalidModifyInfo.add("whatever");
+		_invalidModifyInfo.add("black");
+		_invalidModifyInfo.add("1986");
+		try {
+			_carController.modifyCar(_invalidModifyInfo);
+		} catch (Exception e_) {
+
+		}
+	}
+
+	/**
+	 * Tests CarController's modifyCar method. Ensures that a invalid car cannot be
+	 * modified when car year is invalid.
+	 */
+	@Test
+	public void testModifyInvalidCarYear() {
+		CarController _carController = new CarController();
+
+		// ---------------------------------------------
+		// Creating a valid car to later be modified.
+		try {
+			_carController.createCar(createValidCarInfo());
+		} catch (InvalidCarArgumentsException e_) {
+			fail("Error creating a valid car.");
+		}
+
+		// ---------------------------------------------
+		// Testing invalid car modification.
+		// Trying to change car's year
+		ArrayList<String> _invalidModifyInfo = new ArrayList<String>();
+		_invalidModifyInfo.add("0"); // ID
+		_invalidModifyInfo.add("coupe");
+		_invalidModifyInfo.add("toyota");
+		_invalidModifyInfo.add("trueno");
+		_invalidModifyInfo.add("white");
+		_invalidModifyInfo.add("1999");
+		try {
+			_carController.modifyCar(_invalidModifyInfo);
+		} catch (Exception e_) {
+
+		}
+
+		// -----------------------------------------------
+		// Testing when year is not an integer
+		_invalidModifyInfo.set(5, "notaninteger");
+		try {
+			_carController.modifyCar(_invalidModifyInfo);
+		} catch (Exception e_) {
+
+		}
+	}
+
+	/**
 	 * Tests CarController's deleteCar method. Ensures that a valid car can be
 	 * deleted.
-	 * @throws InvalidCarArgumentsException 
+	 * 
+	 * @throws InvalidCarArgumentsException
 	 */
 	@Test
 	public void testDeleteValidCar() {
@@ -139,10 +358,10 @@ public class CarControllerTest {
 
 	/**
 	 * Tests CarController's deleteCar method. Ensures that a invalid car cannot be
-	 * deleted.
+	 * deleted when input length is invalid
 	 */
 	@Test
-	public void testDeleteInvalidCar() {
+	public void testDeleteInvalidCarLength() {
 		CarController _carController = new CarController();
 
 		// ---------------------------------------------
@@ -156,7 +375,51 @@ public class CarControllerTest {
 		// ---------------------------------------------
 		// Testing invalid car deletion.
 		ArrayList<String> _invalidDeleteInfo = new ArrayList<String>();
+		_invalidDeleteInfo.add("wrong"); // Invalid ID
+		_invalidDeleteInfo.add("length"); // Invalid ID
+		try {
+			_carController.deleteCar(_invalidDeleteInfo);
+		} catch (CarNotFoundException e_) {
+
+		} catch (InvalidCarArgumentsException e_) {
+
+		}
+
+	}
+
+	/**
+	 * Tests CarController's deleteCar method. Ensures that a invalid car cannot be
+	 * deleted when CarId is invalid.
+	 */
+	@Test
+	public void testDeleteInvalidCarID() {
+		CarController _carController = new CarController();
+
+		// ---------------------------------------------
+		// Creating a valid car to later be modified.
+		try {
+			_carController.createCar(createValidCarInfo());
+		} catch (InvalidCarArgumentsException e_) {
+			fail("Error creating a valid car." + e_.getMessage());
+		}
+
+		// ---------------------------------------------
+		// Testing invalid car deletion.
+		// Testing CarId does not exist.
+		ArrayList<String> _invalidDeleteInfo = new ArrayList<String>();
 		_invalidDeleteInfo.add("1000"); // Invalid ID
+		try {
+			_carController.deleteCar(_invalidDeleteInfo);
+		} catch (CarNotFoundException e_) {
+
+		} catch (InvalidCarArgumentsException e_) {
+
+		}
+
+		// ---------------------------------------------
+		// Testing invalid car deletion.
+		// Testing Invalid carID.
+		_invalidDeleteInfo.set(0, "string"); // Invalid ID
 		try {
 			_carController.deleteCar(_invalidDeleteInfo);
 		} catch (CarNotFoundException e_) {
@@ -187,8 +450,43 @@ public class CarControllerTest {
 
 	}
 
+	/**
+	 * Test CarController's GetCardDatabaseAsString method. Ensures that all car in
+	 * the database get printed.
+	 */
+	@Test
+	public void testGetCarDatabaseAsString() {
+		CarController _carController = new CarController();
+
+		// ---------------------------------------------
+		// Creating car to be printed.
+		try {
+			_carController.createCar(createValidCarInfo());
+
+		} catch (InvalidCarArgumentsException e_) {
+			fail("Error creating a valid car.");
+		}
+
+		_carController.getCarDatabaseAsString();
+
+	}
+
+	/**
+	 * Test CarController's GetCardDatabaseAsString method. Ensures that when
+	 * database is empty, the empty string is returned.
+	 */
+	@Test
+	public void testGetCarDatabaseAsEmptyString() {
+		CarController _carController = new CarController();
+
+		// ---------------------------------------------
+		// Test printing the database when it is empty
+		_carController.getCarDatabaseAsString();
+
+	}
+	
 	// ==========================================================================================
-    // Helper Functions
+	// Helper Functions
 
 	/**
 	 * Helper function to generate valid car info.
@@ -213,9 +511,24 @@ public class CarControllerTest {
 	private ArrayList<String> createInvalidCarInfo() {
 		ArrayList<String> _invalidCarInfo = new ArrayList<String>();
 		_invalidCarInfo.add("coupe");
+		_invalidCarInfo.add("BMW");
+		_invalidCarInfo.add("Z4");
+		_invalidCarInfo.add("black");
+		return _invalidCarInfo;
+	}
+
+	/**
+	 * Helper function to generate invalid carType info.
+	 * 
+	 * @return An ArrayList of Strings containing invalid car info.
+	 */
+	private ArrayList<String> createInvalidCarTypeInfo() {
+		ArrayList<String> _invalidCarInfo = new ArrayList<String>();
+		_invalidCarInfo.add("coupee");
 		_invalidCarInfo.add("invalid");
 		_invalidCarInfo.add("input");
-		_invalidCarInfo.add("length");
+		_invalidCarInfo.add("and");
+		_invalidCarInfo.add("more");
 		return _invalidCarInfo;
 	}
 }
